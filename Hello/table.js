7eb8ac1c1bb9c91c8884cceb4a585909f052c226
Iqra327 <iqrarasheed219@gmail.com>
2024-03-06 17:28:12 +0500

    Initial Commit

---
+++ Hello/table.js
@@ -0,0 +1,23 @@
+function generateTable() {
+  let number = document.getElementById('table').value;
+  let limit = document.getElementById('limit').value;
+  let tableHTML = `<table><tr><th> Number </th><th> Result </th></tr>`;
+
+  if (number < 20) {
+    for (var i = limit; i >= 1; i--) {
+      tableHTML += "<tr><td>" + number + " x " + i + "</td><td>" + (number * i) + "</td></tr>";
+    }
+  } else {
+    for (var i = 1; i <= limit; i++) {
+      tableHTML += "<tr><td>" + number + " x " + i + "</td><td>" + (number * i) + "</td></tr>";
+    }
+  }
+
+  tableHTML += "</table>";
+  document.querySelector('.printTable').innerHTML = tableHTML;
+}
+
+let btn = document.getElementById('button1');
+btn.onclick = generateTable;
+
+// generateTable();
